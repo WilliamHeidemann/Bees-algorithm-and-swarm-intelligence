fd68bc8063be4f302297d6fcfba770d3c4715f7f
William Skou Heidemann <Williamskou2227@gmail.com>
2023-04-30 11:00:40 +1000

    attack position refactor

--- Assets/Scripts/DroneScripts/AttackBehaviour.cs
+++ Assets/Scripts/DroneScripts/AttackBehaviour.cs
@@ -5,7 +5,7 @@ namespace DroneScripts
     public class AttackBehaviour : DroneBehaviour
     {
         private readonly Transform _playerTransform;
-        private const float SeparationWeight = 100000;
+        private const float SeparationWeight = 100000f;
         private float _laserTime;
         private const float LaserCooldown = 2f;
         private bool HuntUtility => drone.health > 50;
@@ -50,17 +50,23 @@ namespace DroneScripts
         private void SetHuntPosition()
         {
             var playerPosition = _playerTransform.position;
-            var droneTransform = drone.transform;
-            var distanceVector = droneTransform.position - playerPosition;
-            var separationDirection = distanceVector.normalized;
             var offset = _playerTransform.forward * 200;
-            target = playerPosition + separationDirection / distanceVector.magnitude * SeparationWeight + offset;
+            var distanceVector = drone.transform.position - playerPosition;
+            var playerAvoidance = distanceVector.normalized / distanceVector.magnitude * SeparationWeight;
+            target = playerPosition + offset + playerAvoidance;
         }
 
         protected override void RotateTowardsTarget()
         {
-            var transform = drone.transform;
-            transform.forward = _playerTransform.position - transform.position;
+            if (HuntUtility)
+            {
+                var transform = drone.transform;
+                transform.forward = _playerTransform.position - transform.position;
+            }
+            else
+            {
+                base.RotateTowardsTarget();
+            }
         }
     }
 }
